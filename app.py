191ec3b1564a27747e81010aea20996dcc821c5b
Asset1996 <khaitbay96@gmail.com>
2022-08-16 11:11:56 +0500

    Report message added.

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def index():
             send_message(chat_id, 'Wrong data')
             return Response('OK', status=200)
 
-        # write_json(msg, 'telegram_request.json')
+        write_json(msg, 'telegram_request.json')
         send_message(chat_id, 'Good command')
         return Response('OK, it is POST', status=200)
     else:
